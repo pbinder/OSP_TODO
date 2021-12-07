7131a93c2de7cdcd379ace1b22c3c44ed7cdd06f
cwkim0314 <71135652+cwkim0314@users.noreply.github.com>
2021-12-07 15:42:56 +0900

    added search functions

--- components/Search.js
+++ components/Search.js
@@ -1,21 +1,54 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Text, TextInput} from 'react-native';
+import db from '../firebase';
 
 class Search extends React.Component {
-    render() {
-      return(
-        <View style={styles.overviewContainer}>
-            <View style={styles.Title}>
-                <Text style={styles.todaysTasks}>To Do List</Text>
-            </View>
-            <View style={styles.searchbar}>
-                <Text style={styles.searchitem}>Search</Text>
-            </View>
-        </View>
-      )
-    }
 
+    
+    constructor(){
+      super();
+  
+      this.state={
+        search:null,
+        filteredItems:null
+      };
+    }
+    
+
+    searchSpace=(event)=>{
+        let keyword = event.target.value;
+        this.setState({search:keyword})
+
+        const items = db.filter((data)=>{
+          if(this.state.search == null)
+              return data
+          else if(data.name.toLowerCase().includes(this.state.search.toLowerCase())){
+              return data
+          }
+        })
+
+        this.setState({filteredItems:items})
+        console.log("items",this.state.filteredItems)
+        
+    }
+       
+  
+    render(){
+        
+        return(
+            <View style={styles.overviewContainer}>
+                <View style={styles.Title}>
+                    <Text style={styles.todaysTasks}>To Do List</Text>
+                </View>
+                <View style={styles.searchbar}>
+                <TextInput style={styles.searchitem} placeholder="Search" onChange={(e)=>this.searchSpace(e)}/>
+                {this.state.filteredItems}
+                </View>
+            </View>
+        )   
+    }
 }
+
 
 const styles = StyleSheet.create({
     todaysTasks: {
@@ -47,6 +80,7 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         marginTop: 10,
         paddingLeft: 20,
+        paddingTop: 0,
     },
     searchitem: {
         color: '#C0C0C0',
